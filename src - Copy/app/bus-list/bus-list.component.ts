72f9ba598998800ad5511aafc144704afa4e7b0d
piyali2016 <piyali.acn@gmail.com>
2024-03-15 20:02:17 +0530

    changes push to git

---
+++ src - Copy/app/bus-list/bus-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bus-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './bus-list.component.html',
+  styleUrl: './bus-list.component.css'
+})
+export class BusListComponent {
+
+}
---
+++ src - Copy/app/search-buses/search-buses.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-buses',
+  standalone: true,
+  imports: [],
+  templateUrl: './search-buses.component.html',
+  styleUrl: './search-buses.component.css'
+})
+export class SearchBusesComponent {
+
+}
